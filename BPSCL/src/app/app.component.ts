3e724b553682dde130551eab6b9d0d233c724cf9
Manoj Kumar <manojmani517@gmail.com>
2019-12-15 23:06:10 +0530

    Implemented closing the menu in BPSCL project

--- BPSCL/src/app/app.component.ts
+++ BPSCL/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 import { Platform } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { MenuController } from "@ionic/angular";
 
 @Component({
   selector: "app-root",
@@ -36,9 +37,15 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public menuCtrl: MenuController
   ) {
     this.initializeApp();
+  }
+
+  closeMenu() {
+    // alert("Close the Menu");
+    this.menuCtrl.toggle();
   }
 
   initializeApp() {
