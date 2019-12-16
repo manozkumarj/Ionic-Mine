46ffa8561bbf50fb9c40d058260756adf60b101f
Manoj Kumar <manojkumarzoltglobal@gmail.com>
2019-12-16 18:01:09 +0530

    Added detecting device platform

--- BPSCL/src/app/pages/admin-registration/admin-registration.page.ts
+++ BPSCL/src/app/pages/admin-registration/admin-registration.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-admin-registration',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminRegistrationPage implements OnInit {
 
-  constructor() { }
+  constructor(public plt: Platform) {
+    if (this.plt.is('ios')) {
+      // This will only print when on iOS
+      console.log('I am an iOS device!');
+    } else if ('android') {
+      // This will only print when on Android
+      console.log('I am an Android device!');
+    } else {
+      console.log('Browser');
+    }
+  }
 
   ngOnInit() {
+    // alert(window.cordova.platformId);
   }
 
 }
