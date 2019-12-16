f273479c4241ba5c109429f2b318f8ebb902f436
Manoj Kumar <manojmani517@gmail.com>
2019-12-16 22:52:54 +0530

    Added static loader --> BPSCL

--- BPSCL/src/app/pages/care-provided/care-provided.page.ts
+++ BPSCL/src/app/pages/care-provided/care-provided.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-care-provided',
-  templateUrl: './care-provided.page.html',
-  styleUrls: ['./care-provided.page.scss'],
+  selector: "app-care-provided",
+  templateUrl: "./care-provided.page.html",
+  styleUrls: ["./care-provided.page.scss"]
 })
 export class CareProvidedPage implements OnInit {
+  constructor(public loadingController: LoadingController) {}
 
-  constructor() { }
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: "Please wait...",
+      duration: 5000
+    });
+    await loading.present();
 
-  ngOnInit() {
+    const { role, data } = await loading.onDidDismiss();
+
+    console.log("Loading dismissed!");
   }
 
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 5000,
+      message: "Please wait...",
+      translucent: true,
+      cssClass: "custom-class custom-loading"
+    });
+    return await loading.present();
+  }
+
+  ngOnInit() {
+    this.presentLoading();
+  }
 }
