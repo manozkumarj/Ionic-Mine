f40585905253663958994c02d75f10a162ffdb83
Manoj Kumar <manojkumarzoltglobal@gmail.com>
2019-12-18 18:23:23 +0530

    Started using Database service in Home page --> HomePage-Sqlite-Crud

--- HomePage-Sqlite-Crud/src/app/home/home.page.ts
+++ HomePage-Sqlite-Crud/src/app/home/home.page.ts
@@ -1,97 +1,79 @@
-import { Component } from '@angular/core';
- 
+import { DatabaseService } from './../services/database.service';
+import { Component, OnInit } from '@angular/core';
+
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { Platform } from '@ionic/angular';
- 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
- 
+export class HomePage implements OnInit {
+
   databaseObj: SQLiteObject;
   name_model: string = "";
   row_data: any = [];
   readonly database_name: string = "freaky_datatable.db";
   readonly table_name: string = "myfreakytable";
- 
+
+  showCreateDBBtn: boolean = false;
+  showCreateTableBtn: boolean = false;
+  showGetRowsBtn: boolean = true;
+
   constructor(
     private platform: Platform,
-    private sqlite: SQLite
+    private db: DatabaseService
   ) {
     this.platform.ready().then(() => {
-      this.createDB();
+      this.db.getRows().then(data => {
+        this.row_data = data;
+        alert("Database is exist");
+      }).catch(error => {
+        this.showCreateDBBtn = true;
+        this.showCreateTableBtn = true;
+        this.db.createDB().then(data => {
+          this.db.createTable();
+        }).catch(error => {
+          alert(error);
+        });
+        console.log(error);
+      });
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  getRows() {
+    this.db.getRows().then(data => {
+      this.row_data = data;
+      this.showCreateDBBtn = false;
+      this.showCreateTableBtn = false;
     }).catch(error => {
       console.log(error);
-    })
+    });
   }
- 
- 
-  createDB() {
-    this.sqlite.create({
-      name: this.database_name,
-      location: 'default'
-    })
-      .then((db: SQLiteObject) => {
-        this.databaseObj = db;
-        alert('freaky_datatable Database Created!');
-      })
-      .catch(e => {
-        alert("error " + JSON.stringify(e))
-      });
-  }
- 
-  createTable() {
-    this.databaseObj.executeSql('CREATE TABLE IF NOT EXISTS ' + this.table_name + ' (pid INTEGER PRIMARY KEY, Name varchar(255))', [])
-      .then(() => {
-        alert('Table Created!');
-      })
-      .catch(e => {
-        alert("error " + JSON.stringify(e))
-      });
-  }
- 
-  insertRow() {
-    if (!this.name_model.length) {
+
+  insertRow(name_model) {
+    if (!name_model.length) {
       alert("Enter Name");
       return;
-    }
-    this.databaseObj.executeSql('INSERT INTO ' + this.table_name + ' (Name) VALUES ("' + this.name_model + '")', [])
-      .then(() => {
+    } else {
+      this.db.getRows().then(data => {
+        this.row_data = data;
         alert('Row Inserted!');
-        this.getRows();
-      })
-      .catch(e => {
-        alert("error " + JSON.stringify(e))
       });
+    }
   }
- 
-  getRows() {
-    this.databaseObj.executeSql("SELECT * FROM " + this.table_name, [])
-      .then((res) => {
-        this.row_data = [];
-        if (res.rows.length > 0) {
-          for (var i = 0; i < res.rows.length; i++) {
-            this.row_data.push(res.rows.item(i));
-          }
-        }
-      })
-      .catch(e => {
-        alert("error " + JSON.stringify(e))
-      });
-  }
- 
+
+
   deleteRow(item) {
-    this.databaseObj.executeSql("DELETE FROM " + this.table_name + " WHERE pid = " + item.pid, [])
-      .then((res) => {
-        alert("Row Deleted!");
-        this.getRows();
-      })
-      .catch(e => {
-        alert("error " + JSON.stringify(e))
-      });
+    this.db.getRows().then(data => {
+      this.row_data = data;
+      alert("Row Deleted!");
+    });
   }
- 
- 
+
 }
