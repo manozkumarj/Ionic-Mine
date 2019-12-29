ba7269e02c2f8e51676640fe08db9a1e27d7a8df
Manoj Kumar <manojmani517@gmail.com>
2019-12-29 23:46:07 +0530

    working on DOB, DOB is also almost working --> BPSCL

--- BPSCL/src/app/pages/beneficiary-registration/beneficiary-registration.page.ts
+++ BPSCL/src/app/pages/beneficiary-registration/beneficiary-registration.page.ts
@@ -196,7 +196,10 @@ export class BeneficiaryRegistrationPage implements OnInit {
       .getAgeUnits()
       .then(ageUnits => {
         console.log("Fetched AgeUnits -> " + JSON.stringify(ageUnits));
-        this.ageUnits = ageUnits;
+        this.ageUnits = ageUnits.map(ageUnit => ({
+          ...ageUnit,
+          isSelected: false
+        }));
       })
       .catch(error => {
         console.error(
@@ -327,24 +330,28 @@ export class BeneficiaryRegistrationPage implements OnInit {
     console.log("dateOfBirthChange() - selectedDob -> " + selectedDobSeconds);
     console.log("Date.now() is -> " + Date.now());
 
-    if (selectedDobSeconds && selectedDobSeconds > 0) {
-      var timeDiff = Math.abs(Date.now() - selectedDobSeconds);
-      var diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
-      if (diffDays < 30) {
-        this.benRegForm.patchValue({ ageUnit: 1, age: diffDays });
-      } else if (diffDays < 365) {
-        this.benRegForm.patchValue({
-          ageUnit: 2,
-          age: Math.floor(diffDays / 30)
-        });
-      } else {
-        this.benRegForm.patchValue({
-          ageUnit: 3,
-          age: Math.floor(diffDays / 365)
-        });
-        console.log("asigning ageUnit & age here ");
-      }
-    }
+    // if (selectedDobSeconds && selectedDobSeconds > 0) {
+    //   var timeDiff = Math.abs(Date.now() - selectedDobSeconds);
+    //   var diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+    //   if (diffDays < 30) {
+    //     this.benRegForm.patchValue({ ageUnit: 1, age: diffDays });
+    //   } else if (diffDays < 365) {
+    //     this.benRegForm.patchValue({
+    //       ageUnit: 2,
+    //       age: Math.floor(diffDays / 30)
+    //     });
+    //   } else {
+    //     this.benRegForm.patchValue({
+    //       ageUnit: 3,
+    //       age: Math.floor(diffDays / 365)
+    //     });
+    //     this.benRegForm.get("ageUnit").setValue(3);
+    //     console.log("asigning ageUnit & age here ");
+    //   }
+    //   console.log(
+    //     "ageUnit was set and value is " + this.benRegForm.get("ageUnit").value
+    //   );
+    // }
   }
 
   onSubmit(values) {
