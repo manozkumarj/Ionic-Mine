f6f8dacaddcbb24d1060f85a733dcb3f1ff7a7e4
Manoj Kumar <manojmani517@gmail.com>
2020-01-02 23:47:01 +0530

    Working on Consumable Dispense, Medicine Dispense pages static things - Dynamic fields --> BPSCL

--- BPSCL/src/app/pages/consumable-dispense/consumable-dispense.page.ts
+++ BPSCL/src/app/pages/consumable-dispense/consumable-dispense.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-consumable-dispense',
-  templateUrl: './consumable-dispense.page.html',
-  styleUrls: ['./consumable-dispense.page.scss'],
+  selector: "app-consumable-dispense",
+  templateUrl: "./consumable-dispense.page.html",
+  styleUrls: ["./consumable-dispense.page.scss"]
 })
 export class ConsumableDispensePage implements OnInit {
+  showDispenses: boolean = false;
 
-  constructor() { }
+  consumableDispenses: any[] = [
+    {
+      itemId: 1,
+      genericName: "Bandage 6 inch",
+      allowQuantity: false
+    },
+    {
+      itemId: 2,
+      genericName: "Cotton Role 450grm",
+      allowQuantity: false
+    },
+    {
+      itemId: 3,
+      genericName: "Face mask",
+      allowQuantity: false
+    }
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  remarksCheckbox(e) {
+    if (e.target.checked) {
+      this.showDispenses = true;
+      // this.doctorForm.patchValue({ cd: "N/A", ncd: "N/A", minorAilments: "N/A", refferedTo: -1 });
+      console.log("remarksCheckbox is checked");
+    } else {
+      this.showDispenses = false;
+      // this.doctorForm.patchValue({ cd: "", ncd: "", minorAilments: "", refferedTo: "" });
+      console.log("remarksCheckbox is unchecked");
+    }
   }
 
+  dispenseCheckbox(id, e) {
+    if (e.target.checked) {
+      this.consumableDispenses[id - 1]["allowQuantity"] = true;
+      console.log(id + " -> dispenseCheckbox is checked");
+    } else {
+      this.consumableDispenses[id - 1]["allowQuantity"] = false;
+      console.log(id + " -> dispenseCheckbox is unchecked");
+    }
+  }
 }
