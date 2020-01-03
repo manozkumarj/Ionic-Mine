d09bb482eb911788d2d98f01b13efbbc776d5c2a
Manoj Kumar <manojmani517@gmail.com>
2020-01-03 23:57:59 +0530

    Working on static pages & forms basic validations  --> BPSCL

--- BPSCL/src/app/pages/beneficiary-history/beneficiary-history.page.ts
+++ BPSCL/src/app/pages/beneficiary-history/beneficiary-history.page.ts
@@ -1,32 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Validators, FormGroup, FormControl } from "@angular/forms";
 import { DatabaseService } from "src/app/services/database.service";
 import { StorageService } from "./../../services/storage.service";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-beneficiary-history',
-  templateUrl: './beneficiary-history.page.html',
-  styleUrls: ['./beneficiary-history.page.scss'],
+  selector: "app-beneficiary-history",
+  templateUrl: "./beneficiary-history.page.html",
+  styleUrls: ["./beneficiary-history.page.scss"]
 })
 export class BeneficiaryHistoryPage implements OnInit {
+  benHistoryForm: FormGroup;
+  selectedBenId = 0;
+  selectedHistoryId = 0;
 
-  searchBenForm: FormGroup;
   results: any[] = [
     {
-      visitDate: '2019-12-29',
-      testName: 'test',
-      result: 'jumbo'
+      visitDate: "2019-12-29",
+      testName: "test",
+      result: "jumbo"
     }
   ];
 
   constructor(
     private db: DatabaseService,
     private router: Router,
-    private storageService: StorageService) {
-    this.searchBenForm = new FormGroup({
-      benificiaryName: new FormControl("", Validators.required),
-      numberOfFamilyMembers: new FormControl("", Validators.required)
+    private storageService: StorageService
+  ) {
+    this.benHistoryForm = new FormGroup({
+      beneficiaryId: new FormControl("", Validators.required),
+      beneficiaryHistory: new FormControl("", Validators.required)
     });
   }
 
@@ -34,19 +37,43 @@ export class BeneficiaryHistoryPage implements OnInit {
     // this.loadBeneficiaries();
   }
 
+  onChange() {
+    let selectedBenId = this.benHistoryForm.get("beneficiaryId").value;
+    let selectedHistoryId = this.benHistoryForm.get("beneficiaryHistory").value;
+
+    if (!selectedBenId || selectedBenId <= 0) {
+      alert("Please select Beneficiary ID");
+      return false;
+    }
+    if (!selectedHistoryId || selectedHistoryId <= 0) {
+      alert("Please select Beneficiary History");
+      return false;
+    }
+
+    this.onSubmit(this.benHistoryForm.value);
+  }
+
   loadBeneficiaries() {
     this.db
       .getBeneficiaries()
       .then(beneficiaries => {
-        console.log("Fetched beneficiaries -> " + JSON.stringify(beneficiaries));
+        console.log(
+          "Fetched beneficiaries -> " + JSON.stringify(beneficiaries)
+        );
         this.results = beneficiaries;
       })
       .catch(error => {
         console.error(
           "Error -> getBeneficiaries() function returned error." +
-          JSON.stringify(error)
+            JSON.stringify(error)
         );
       });
   }
 
+  onSubmit(values) {
+    console.log("Ben Registration form is submitted, below are the values");
+    console.log(values);
+
+    alert("Form can be submitted");
+  }
 }
--- BPSCL/src/app/pages/search-beneficiary/search-beneficiary.page.ts
+++ BPSCL/src/app/pages/search-beneficiary/search-beneficiary.page.ts
@@ -1,35 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Validators, FormGroup, FormControl } from "@angular/forms";
 import { DatabaseService } from "src/app/services/database.service";
 import { StorageService } from "./../../services/storage.service";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-search-beneficiary',
-  templateUrl: './search-beneficiary.page.html',
-  styleUrls: ['./search-beneficiary.page.scss'],
+  selector: "app-search-beneficiary",
+  templateUrl: "./search-beneficiary.page.html",
+  styleUrls: ["./search-beneficiary.page.scss"]
 })
 export class SearchBeneficiaryPage implements OnInit {
-
   searchBenForm: FormGroup;
-  beneficiaries: any[] = [
-    {
-      imageUrl: 'assets/profile_pic.jpg',
-      patientId: 'SP0002000002B00TEST',
-      name: 'test',
-      surname: 'jumbo',
-      gender: 'Female',
-      age: 25
-    }
-  ];
+  beneficiaries: any[] = [];
 
   constructor(
     private db: DatabaseService,
     private router: Router,
-    private storageService: StorageService) {
+    private storageService: StorageService
+  ) {
     this.searchBenForm = new FormGroup({
-      benificiaryName: new FormControl("", Validators.required),
-      numberOfFamilyMembers: new FormControl("", Validators.required)
+      beneficiaryId: new FormControl("", Validators.required),
+      beneficiaryName: new FormControl("", Validators.required),
+      beneficiarySurname: new FormControl("", Validators.required),
+      beneficiaryAge: new FormControl("", Validators.required),
+      beneficiaryAgeUnit: new FormControl("", Validators.required),
+      beneficiaryGender: new FormControl("", Validators.required)
     });
   }
 
@@ -41,15 +36,57 @@ export class SearchBeneficiaryPage implements OnInit {
     this.db
       .getBeneficiaries()
       .then(beneficiaries => {
-        console.log("Fetched beneficiaries -> " + JSON.stringify(beneficiaries));
+        console.log(
+          "Fetched beneficiaries -> " + JSON.stringify(beneficiaries)
+        );
         this.beneficiaries = beneficiaries;
       })
       .catch(error => {
         console.error(
           "Error -> getBeneficiaries() function returned error." +
-          JSON.stringify(error)
+            JSON.stringify(error)
         );
       });
   }
 
+  onSubmit(values) {
+    console.log("Search Ben form is submitted, below are the values");
+    console.log(values);
+
+    let beneficiaryId = this.searchBenForm.get("beneficiaryId").value;
+    let beneficiaryName = this.searchBenForm
+      .get("beneficiaryName")
+      .value.trim();
+    let beneficiarySurname = this.searchBenForm
+      .get("beneficiarySurname")
+      .value.trim();
+    let beneficiaryAge = this.searchBenForm.get("beneficiaryAge").value;
+    let beneficiaryAgeUnit = this.searchBenForm.get("beneficiaryAgeUnit").value;
+    let beneficiaryGender = this.searchBenForm.get("beneficiaryGender").value;
+
+    if (
+      !beneficiaryId &&
+      !beneficiaryName &&
+      !beneficiarySurname &&
+      !beneficiaryAge &&
+      !beneficiaryAgeUnit &&
+      !beneficiaryGender
+    ) {
+      alert("Please fill any one the fields.");
+      return false;
+    }
+
+    this.beneficiaries = [
+      {
+        imageUrl: "assets/profile_pic.jpg",
+        patientId: "SP0002000002B00TEST",
+        name: "test",
+        surname: "jumbo",
+        gender: "Female",
+        age: 25
+      }
+    ];
+
+    alert("Form can be submitted");
+  }
 }
