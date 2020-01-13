21ef68822803060d7bd28bd8e66cd7e22abd6d07
Manoj Kumar <manojmani517@gmail.com>
2020-01-13 14:00:12 +0530

    Notifications are triggering but not showing like usual push notifications.  --> Mixing-local-notifications-and-backgroundmode

--- Mixing-local-notifications-and-backgroundmode/src/app/home/home.page.ts
+++ Mixing-local-notifications-and-backgroundmode/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Platform } from "@ionic/angular";
 import { BackgroundMode } from "@ionic-native/background-mode/ngx";
+import { LocalNotifications } from "@ionic-native/local-notifications/ngx";
 
 @Component({
   selector: "app-home",
@@ -8,20 +9,35 @@ import { BackgroundMode } from "@ionic-native/background-mode/ngx";
   styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  notificationAlreadyReceived = false;
   constructor(
     public backgroundMode: BackgroundMode,
-    public platform: Platform
+    public platform: Platform,
+    public localNotifications: LocalNotifications
   ) {
     platform.ready().then(() => {
       this.backgroundMode.on("activate").subscribe(() => {
-        console.log("Device is in background");
+        console.log("Device is in background... :)");
+        if (this.notificationAlreadyReceived === false) {
+          this.showNotification();
+        }
       });
-      
+
       this.backgroundMode.on("deactivate").subscribe(() => {
-        console.log("Device is in foreground");
+        console.log("Device is in foreground...");
+        this.showNotification();
       });
 
       this.backgroundMode.enable();
     });
+  }
+
+  showNotification() {
+    this.localNotifications.schedule({
+      text: "There is a legendary Pokemon near you",
+      foreground: true
+    });
+
+    this.notificationAlreadyReceived = true;
   }
 }
