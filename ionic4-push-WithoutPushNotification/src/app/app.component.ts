14e0f983f7ca353a1c11866a02cee75d03bf8aad
Manoj Kumar <manojmani517@gmail.com>
2020-01-13 19:03:11 +0530

    Added 'ionic4-push-WithoutPushNotification' project, Initial commit  --> ionic4-push-WithoutPushNotification

---
+++ ionic4-push-WithoutPushNotification/src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from "@angular/core";
+import { FCM } from "@ionic-native/fcm/ngx";
+import { Router } from "@angular/router";
+
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: "Home",
+      url: "/home",
+      icon: "home"
+    },
+    {
+      title: "List",
+      url: "/list",
+      icon: "list"
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private fcm: FCM,
+    private router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+
+      this.fcm.getToken().then(token => {
+        console.log(token);
+      });
+
+      this.fcm.onTokenRefresh().subscribe(token => {
+        console.log(token);
+      });
+    });
+
+    this.fcm.onNotification().subscribe(data => {
+      console.log(data);
+      if (data.wasTapped) {
+        console.log("Received in background");
+        this.router.navigate([data.landing_page, data.price]);
+      } else {
+        console.log("Received in foreground");
+        this.router.navigate([data.landing_page, data.price]);
+      }
+    });
+  }
+}
