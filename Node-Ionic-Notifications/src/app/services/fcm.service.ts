1af58299255845a5a7138ab39d672ecfc489c950
Manoj Kumar <manojmani517@gmail.com>
2020-01-15 12:34:18 +0530

    Modified code --> Node-Ionic-Notifications

--- Node-Ionic-Notifications/src/app/services/fcm.service.ts
+++ Node-Ionic-Notifications/src/app/services/fcm.service.ts
@@ -1,44 +1,49 @@
-import { Injectable } from '@angular/core';
-import { Firebase } from '@ionic-native/firebase/ngx';
+import { Injectable } from "@angular/core";
+import { Firebase } from "@ionic-native/firebase/ngx";
 import { Platform } from "@ionic/angular";
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class FcmServices {
-
-  constructor(private platform: Platform,
+  constructor(
+    private platform: Platform,
     public firebaseNative: Firebase,
-    public http: HttpClient) {
-    console.log('Hello FcmProvider Provider');
+    public http: HttpClient
+  ) {
+    console.log("Hello FcmProvider Provider");
   }
+
+  ipAddress = "192.168.43.22";
 
   // Get permission from the user
   async getToken() {
     let token;
 
-    if (this.platform.is('android')) {
-      token = await this.firebaseNative.getToken()
+    if (this.platform.is("android")) {
+      token = await this.firebaseNative.getToken();
     }
 
-    if (this.platform.is('ios')) {
+    if (this.platform.is("ios")) {
       token = await this.firebaseNative.getToken();
       await this.firebaseNative.grantPermission();
     }
     console.log("Token is -> " + token);
     // Post the token to your node server
-    this.http.post("http://<ip_address_of_node_server>:6789/store", token)
-      .subscribe(data => {
+    this.http.post(`${this.ipAddress}:6688/storeToken`, token).subscribe(
+      data => {
         console.log(JSON.stringify(data));
-      }, error => {
+      },
+      error => {
         console.log("err");
         console.log(JSON.stringify(error));
-      });
+      }
+    );
   }
 
   // Listen to incoming FCM messages
   listenToNotifications() {
-    return this.firebaseNative.onNotificationOpen()
+    return this.firebaseNative.onNotificationOpen();
   }
 }
