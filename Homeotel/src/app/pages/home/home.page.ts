b80f3584899313cc6ca2d762f7d0bd89a7a2eb6d
Manoj Kumar <manojkumarzoltglobal@gmail.com>
2020-02-24 11:50:01 +0530

    Home page is almost completed, only Family section style is pending --> Homeotel

--- Homeotel/src/app/pages/home/home.page.ts
+++ Homeotel/src/app/pages/home/home.page.ts
@@ -8,6 +8,24 @@ import { CommonService } from "./../../services/common.service";
 })
 export class HomePage implements OnInit {
 
+  healthTips = [
+    {
+      avatar: 'https://m.media-amazon.com/images/M/MV5BNDdhMzMxOTctNDMyNS00NTZmLTljNWEtNTc4MDBmZTYxY2NmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      time: '150024551241'
+    },
+    {
+      avatar: 'https://m.media-amazon.com/images/M/MV5BZTEyMTlmYTUtMDE0My00NWJmLWJlZmEtNzhjNGI0Mzk0NTA4XkEyXkFqcGdeQXVyNTkzNDQ4ODc@._V1_SX300.jpg',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      time: '150024551242'
+    },
+    {
+      avatar: 'https://m.media-amazon.com/images/M/MV5BYzM0ZTg2OTEtNzI4My00NjBlLWFhYTctY2E4NzdiYzY1YWYwXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      time: '150024551243'
+    }
+  ];
+
   constructor(private commonService: CommonService) { }
 
   ngOnInit() {
