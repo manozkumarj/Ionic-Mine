161744dc495938add06df3e3a749386848180ac9
Manoj Kumar <manojkumarzoltglobal@gmail.com>
2020-03-13 12:57:37 +0530

    Implemented dummy form in 'Alert' in 'my-doctors' page --> Homeotel

--- Homeotel/src/app/pages/my-doctors/my-doctors.page.ts
+++ Homeotel/src/app/pages/my-doctors/my-doctors.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: 'app-my-doctors',
@@ -7,26 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyDoctorsPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
   inputKeyUp(e) {
     var target = e.srcElement;
-    var maxLength = parseInt(target.attributes["maxlength"].value, 6);
+    var maxLength = parseInt(target.attributes["maxlength"].value, 5);
     var myLength = target.value.length;
     if (myLength >= maxLength) {
       var next = target;
       while (next = next.nextElementSibling) {
-        if (next == null)
+        if (next == null) {
+          this.showPromptAlert();
           break;
+        }
         if (next.tagName.toLowerCase() == "input") {
           next.focus();
           break;
         }
       }
     }
+    this.showPromptAlert();
+  }
+
+  showPromptAlert() {
+    let alert = this.alertCtrl
+      .create({
+        header: 'Login',
+        inputs: [
+          {
+            name: 'username',
+            placeholder: 'Enter Username'
+          },
+          {
+            name: 'password',
+            placeholder: 'Enter Password',
+            type: 'password'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: data => {
+              console.log('You Clicked on Cancel');
+            }
+          },
+          {
+            text: 'Login',
+            handler: data => {
+              if (data.username, data.password) {
+                console.log('Form is valid');
+                console.log(`
+                username -> ${data.username}
+                password -> ${data.password}
+                `);
+              } else {
+                console.log('Form is invalid');
+                return false;
+              }
+            }
+          }
+        ]
+      }).then(alert1 => alert1.present());
   }
 
 }
