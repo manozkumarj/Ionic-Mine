9e636db9ed775f1c5482ef7e1450f58ec2627698
Manoj Kumar <manojmani517@gmail.com>
2020-03-27 09:05:13 +0530

    Added 'Ionic4FireBase' project with fireStore functionality --> Ionic4FireBase

---
+++ Ionic4FireBase/src/app/add-note/add-note.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+import { FirebaseService } from "../services/firebase.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastController } from "@ionic/angular";
+import { Note } from "../modal/Note";
+
+@Component({
+  selector: "app-add-note",
+  templateUrl: "./add-note.page.html",
+  styleUrls: ["./add-note.page.scss"]
+})
+export class AddNotePage implements OnInit {
+  note: Note = {
+    title: "",
+    content: "",
+    createdAt: new Date().getTime()
+  };
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private fbService: FirebaseService,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) {}
+
+  ngOnInit() {}
+
+  addNote() {
+    console.log("addNote func triggered...");
+    this.fbService.addNote(this.note).then(
+      () => {
+        this.router.navigateByUrl("/");
+      },
+      err => {}
+    );
+  }
+}
---
+++ Ionic4FireBase/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAWBi25xvF0qbfkVVllf5tXMjqM0_JseZ8",
+    authDomain: "ionic4firebase-579cd.firebaseapp.com",
+    databaseURL: "https://ionic4firebase-579cd.firebaseio.com",
+    projectId: "ionic4firebase-579cd",
+    storageBucket: "ionic4firebase-579cd.appspot.com",
+    messagingSenderId: "506779763867",
+    appId: "1:506779763867:web:ab4fee78a6a9cc4e65723e",
+    measurementId: "G-EZEHDRZVH6"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
