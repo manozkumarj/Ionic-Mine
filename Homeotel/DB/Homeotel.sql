4acff5fb0a8eec72d190b86a3b4119a34f93b439
Manoj Kumar <manojmani517@gmail.com>
2020-04-09 15:06:49 +0530

    Started working on Files pages. Created SP to get Files data & master as well --> Homeotel

--- Homeotel/DB/Homeotel.sql
+++ Homeotel/DB/Homeotel.sql
@@ -1119,17 +1119,22 @@ INSERT INTO `m_exercise` (`exercise_id`,`name`,`is_active`) VALUES
 
 DROP TABLE IF EXISTS `m_file_type`;
 CREATE TABLE `m_file_type` (
-  `file_type_id` int(10) unsigned NOT NULL,
+  `file_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
+  `icon` varchar(100) NOT NULL,
   `is_active` varchar(45) NOT NULL,
   PRIMARY KEY (`file_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `m_file_type`
 --
 
 /*!40000 ALTER TABLE `m_file_type` DISABLE KEYS */;
+INSERT INTO `m_file_type` (`file_type_id`,`name`,`icon`,`is_active`) VALUES 
+ (1,'Prescription','newspaper-outline','1'),
+ (2,'Labtest','library-outline','1'),
+ (3,'Bill','receipt-outline','1');
 /*!40000 ALTER TABLE `m_file_type` ENABLE KEYS */;
 
 
@@ -2170,6 +2175,28 @@ END;
  SELECT * FROM du_doctor du LEFT JOIN d_doctor d ON du.doctor_id = d.id where du.user_id=IN_userId;
 
  END $$
+/*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
+
+DELIMITER ;
+
+--
+-- Definition of procedure `sp_user_files_get`
+--
+
+DROP PROCEDURE IF EXISTS `sp_user_files_get`;
+
+DELIMITER $$
+
+/*!50003 SET @TEMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_user_files_get`(IN IN_user_id INT)
+BEGIN
+
+  SELECT f.relative_id, f.file_type_id, f.file_date, f.file_blob, mf.name FROM ehr_file f
+  LEFT JOIN m_file_type mf ON f.file_type_id = mf.file_type_id WHERE f.user_id = IN_user_id;
+
+  SELECT * FROM m_file_type WHERE is_active = 1;
+
+END $$
 /*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
 
 DELIMITER ;
