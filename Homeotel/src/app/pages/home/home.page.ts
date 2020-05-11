e5af76487ac102e6304523affcbbb73bb7747b1a
Manoj Kumar <manojmani517@gmail.com>
2020-05-11 13:55:51 +0530

    Modified appointments page  --> Homeotel

--- Homeotel/src/app/pages/home/home.page.ts
+++ Homeotel/src/app/pages/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonService } from "./../../services/common.service";
+import { ApiService } from "src/app/services/api.service";
+import { UtilitiesService } from "src/app/services/utilities.service";
 
 @Component({
   selector: "app-home",
@@ -7,31 +9,37 @@ import { CommonService } from "./../../services/common.service";
   styleUrls: ["./home.page.scss"],
 })
 export class HomePage implements OnInit {
-  healthTips = [
-    {
-      avatar:
-        "https://m.media-amazon.com/images/M/MV5BNDdhMzMxOTctNDMyNS00NTZmLTljNWEtNTc4MDBmZTYxY2NmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      time: "150024551241",
-    },
-    {
-      avatar:
-        "https://m.media-amazon.com/images/M/MV5BZTEyMTlmYTUtMDE0My00NWJmLWJlZmEtNzhjNGI0Mzk0NTA4XkEyXkFqcGdeQXVyNTkzNDQ4ODc@._V1_SX300.jpg",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      time: "150024551242",
-    },
-    {
-      avatar:
-        "https://m.media-amazon.com/images/M/MV5BYzM0ZTg2OTEtNzI4My00NjBlLWFhYTctY2E4NzdiYzY1YWYwXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      time: "150024551243",
-    },
-  ];
+  allAppointments: any[] = [];
 
-  constructor(private commonService: CommonService) {}
+  constructor(
+    private commonService: CommonService,
+    private apiService: ApiService,
+    private utilities: UtilitiesService
+  ) {
+    this.getAppointments();
+  }
+
+  getAppointments() {
+    this.apiService.getAppointments().subscribe((data) => {
+      console.log("Returned from Backend");
+      console.log(data);
+      console.log(data[0]);
+      if (this.utilities.isInvalidApiResponseData(data)) {
+        console.log("Returned Error");
+      } else {
+        if (
+          typeof data != "undefined" &&
+          typeof data[0] != "undefined" &&
+          typeof data[0][0] != "undefined"
+        ) {
+          this.allAppointments = data[0];
+          console.log("Appointments found");
+        } else {
+          console.log("Backend returned error");
+        }
+      }
+    });
+  }
 
   ngOnInit() {}
 
